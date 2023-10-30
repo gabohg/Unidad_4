acb1e25ac69dda3d4900da6b23dc566c8eaf3e23
gabohg <143622336+gabohg@users.noreply.github.com>
2023-10-30 09:00:01 -0600

    Add files via upload

---
+++ Proyecto2.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto.pkg2;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Proyecto2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Escribe le numero que desees");
+        int numRows = sc.nextInt();
+        
+        if (true){
+        
+            System.out.println("1,AB,234,CDEF,56789,GHIJKL,1234567");
+         
+        }else {
+            System.out.println("1,AB,234,CDEF,56789,GHIJKL,1234567");
+        }
+        
+            System.out.println("");
+    }
+    
+}
